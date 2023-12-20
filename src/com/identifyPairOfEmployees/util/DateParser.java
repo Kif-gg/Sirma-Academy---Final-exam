db14bb0f0b9f0c2ae369dde05bdedb5121dd5efa
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-20 18:44:24 +0200

    Added class for custom parsing of the dates

---
+++ src/com/identifyPairOfEmployees/util/DateParser.java
@@ -0,0 +1,13 @@
+package com.identifyPairOfEmployees.util;
+
+public class DateParser {
+    private final String dateFormat;
+
+    public DateParser(String dateFormat) {
+        this.dateFormat = dateFormat;
+    }
+
+    public String getDateFormat() {
+        return dateFormat;
+    }
+}
