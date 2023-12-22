2490993bad7435d047d06f2ea2cece7f99789693
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-22 09:56:21 +0200

    Modified reader's date parsing, added line saving feature

--- src/com/identifyPairOfEmployees/fileManager/CSVReader.java
+++ src/com/identifyPairOfEmployees/fileManager/CSVReader.java
@@ -2,34 +2,33 @@ package com.identifyPairOfEmployees.fileManager;
 
 import com.identifyPairOfEmployees.Main;
 import com.identifyPairOfEmployees.models.AssignedProject;
+import com.identifyPairOfEmployees.models.CSVLine;
 import com.identifyPairOfEmployees.models.Employee;
 
 import java.io.*;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class CSVReader {
     public static ArrayList<Employee> read() {
         ArrayList<Employee> employees = new ArrayList<>();
+        ArrayList<CSVLine> lines = new ArrayList<>();
         String path = "src\\com\\identifyPairOfEmployees\\database\\data.csv";
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line;
             br.readLine(); // Skipping the headers of CSV file
             while ((line = br.readLine()) != null) {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(Main.dateParser.getDateFormat());
                 String[] values = line.split(", ");
                 int empID = Integer.parseInt(values[0]);
                 int projId = Integer.parseInt(values[1]);
-                LocalDate dateFrom = LocalDate.parse(values[2], formatter);
+                LocalDate dateFrom = LocalDate.parse(values[2]);
                 LocalDate dateTo;
                 if (values[3].equals("NULL")) {
                     LocalDate dateToInit = LocalDate.now();
-                    dateTo = LocalDate.parse(dateToInit.format(formatter));
+                    dateTo = LocalDate.parse(dateToInit.toString());
                 } else {
-                    dateTo = LocalDate.parse(values[3], formatter);
+                    dateTo = LocalDate.parse(values[3]);
                 }
-
                 AssignedProject assignedProject = new AssignedProject(projId, dateFrom, dateTo);
 
                 Employee searchEmployee = findEmployeeById(empID, employees);
@@ -40,10 +39,12 @@ public class CSVReader {
                 } else {
                     searchEmployee.addAssignedProject(assignedProject);
                 }
+                lines.add(new CSVLine(empID, projId, dateFrom, dateTo));
             }
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+        Main.lines = lines;
         return employees;
     }
 
