fdaedcccb7676cc497694eaf7db12d8a9858e79f
Akif Syuleyman <f17.8oy@gmail.com>
2023-12-22 09:54:00 +0200

    Modified entry point with single scanner for app, saving changes to database

--- src/com/identifyPairOfEmployees/Main.java
+++ src/com/identifyPairOfEmployees/Main.java
@@ -1,22 +1,39 @@
 package com.identifyPairOfEmployees;
 
+import com.identifyPairOfEmployees.fileManager.CSVWriter;
+import com.identifyPairOfEmployees.models.CSVLine;
 import com.identifyPairOfEmployees.models.Employee;
 import com.identifyPairOfEmployees.services.InputHandler;
 import com.identifyPairOfEmployees.util.DateParser;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Main {
     public static DateParser dateParser;
     public static ArrayList<Employee> employees;
+    public static ArrayList<CSVLine> lines;
+    public static boolean changesMade = false;
 
     public static void main(String[] args) {
-        startApp();
-        InputHandler.printResult();
+        try (Scanner sc = new Scanner(System.in)) {
+            startApp(sc);
+            if (changesMade) {
+                System.out.println("CHANGES DETECTED! Saving...");
+                CSVWriter.write();
+                System.out.println("Changes saved successfully!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void startApp() {
-        dateParser = InputHandler.createDateParser();
+    public static void startApp(Scanner sc) {
+        dateParser = InputHandler.createDateParser(sc);
+        if (dateParser == null) {
+            return;
+        }
+        InputHandler.manageMainMenu(sc);
     }
 }
